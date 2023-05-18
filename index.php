b9a1d3749e483a9386b17624dab0f8aaba6cd914
Yori <yusufmj605@gmail.com>
2023-05-18 23:42:11 +0300

    using require for reusing the nav

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require("reuse/nav.html");?>
+
+<div class="container-fluid">
+                    <div class="row secondary">
+                        <div class="col-12 col-md-6 d-flex align-items-center">
+                           <section>
+                            <div class="row text-center ">
+                                <div class="col"><h1>Welcome to Nintendo!</h1></div>
+                            </div>
+                            <div class="row">  
+                                <div class="col-8 mx-auto"><p>
+                                    we are very intrigued in your opinion of us and our works.
+                                if you share with us your opinions,
+                                 we would be able to improve even further to enhance your experience with us!
+                                click here to answer our Questionnaires! :)
+                                </p></div>
+                            </div>  
+                           </section>
+                            
+                             
+                            
+                        </div>
+                        <div class="col d-none d-md-block">
+                        <img src="marioPeek.webp" alt="marioPeek" class="responsiveIMG">
+
+                        </div>
+                    </div>
+                </div>
+            
+              <footer>
+                a
+              </footer>
+
+
+<?php
+require("reuse/end.html");
+?>
