86fabaf5d3d0ea3380f5ee1632eaa6ccfe166cc3
Yori <yusufmj605@gmail.com>
2023-05-19 01:00:49 +0300

    added an initial login page design

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<?php
-require("reuse/nav.html");?>
+<?php require("reuse/nav.html");?>
 
 <div class="container-fluid">
-                    <div class="row secondary">
-                        <div class="col-12 col-md-6 d-flex align-items-center">
+            <div class="row secondary">
+                    <div class="col-12 col-md-6 d-flex align-items-center">
                            <section>
+
                             <div class="row text-center ">
                                 <div class="col"><h1>Welcome to Nintendo!</h1></div>
                             </div>
+
                             <div class="row">  
                                 <div class="col-8 mx-auto"><p>
                                     we are very intrigued in your opinion of us and our works.
@@ -15,24 +16,25 @@ require("reuse/nav.html");?>
                                  we would be able to improve even further to enhance your experience with us!
                                 click here to answer our Questionnaires! :)
                                 </p></div>
-                            </div>  
+                            </div>
+
                            </section>
                             
                              
                             
                         </div>
+
                         <div class="col d-none d-md-block">
                         <img src="marioPeek.webp" alt="marioPeek" class="responsiveIMG">
 
-                        </div>
                     </div>
-                </div>
+            </div>
+</div>
             
               <footer>
-                a
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores vero facere odio provident error. Laborum id,
+                ratione sint excepturi reprehenderit sed culpa iste, expedita quae soluta voluptatem at, totam accusamus!
               </footer>
 
 
-<?php
-require("reuse/end.html");
-?>
+<?php require("reuse/end.html");?>
