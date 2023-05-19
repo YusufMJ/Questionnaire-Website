57468477ad8af4fec328e96bcc1bc7af0706822e
Yori <yusufmj605@gmail.com>
2023-05-19 18:25:08 +0300

    finished the animation part maybe

--- questionnaireList.php
+++ questionnaireList.php
@@ -1,11 +1,11 @@
 <?php require("reuse/nav.html");?>
 <h1 class="text-center mt-5">Questionnaire Lists!</h1>
-<p class="text-center mt-5">Feel free to answer our Questionnaires :]</p> 
+<p class="text-center mt-5">Welcome {username}! Feel free to answer our Questionnaires :]</p> 
 
 <div class="container">
-  <div class="row" id="qLparent">
-    <div class="col-12 col-md-6 d-flex" id="qL1">
-      <button class="btn btn-primary primary flex-fill" type="button" data-bs-toggle="collapse"
+  <div class="row my-5 d-flex justify-content-center" id="qLparent">
+    <div class="col-12 col-md-6 d-flex mb-5" id="qL1">
+      <button class="btn btn-outline-dark textColor flex-fill" type="button" data-bs-toggle="collapse"
       data-bs-target="#Zelda" aria-expanded="false" aria-controls="Zelda">
         Zelda
         <div class="collapse" id="Zelda">
@@ -15,8 +15,8 @@
         </div>
       </button>
     </div>
-    <div class="col-12 col-md-6 d-flex">
-      <button class="btn btn-primary primary flex-fill" type="button" data-bs-toggle="collapse"
+    <div class="col-12 col-md-6 d-flex mb-5">
+      <button class="btn btn-outline-dark flex-fill" type="button" data-bs-toggle="collapse"
       data-bs-target="#mario" aria-expanded="false" aria-controls="mario">
         Mario
         <div class="collapse" id="mario">
