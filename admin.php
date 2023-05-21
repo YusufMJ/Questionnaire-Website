d894e0f8b2af6189d8c44389a890189ba5968d0b
Yori <yusufmj605@gmail.com>
2023-05-22 01:33:33 +0300

    made the admin buttons more responsive

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 
 
 <div class="container">
-        <main class="formB">
+        <main class="formB ">
             <form action="">
                 <div>
                     <br>
@@ -36,26 +36,48 @@
         </main>
         <footer class="formBtn sticky-bottom">
     <br>
-        <div class="row btn-group d-flex justify-content-center px-5 p-md-0">
-            <div class="col-12 col-md-2 col-sm-6">
+        <div class="row btn-group d-none d-md-flex justify-content-center px-5 p-md-0">
+            <div class="col-12 col-sm-6 col-md-2">
                 <button class="btn btn-outline-light btn-lg btn-size">Likert</button>
             </div>
-            <div class="col-12 col-md-2 col-sm-6">
+            <div class="col-12 col-sm-6 col-md-2">
                 <button class="btn btn-outline-light btn-lg btn-size">T/F</button>
             </div>
-            <div class="col-12 col-md-2 col-sm-6">
+            <div class="col-12 col-sm-6 col-md-2">
                 <button class="btn btn-outline-light btn-lg btn-size">MCQ</button>
             </div>
-            <div class="col-12 col-md-2 col-sm-6">
+            <div class="col-12 col-sm-6 col-md-2">
                 <button class="btn btn-outline-light text-nowrap btn-lg btn-size">Short</button>
             </div>
         </div>
-        <div class="row d-flex justify-content-center  justify-content-md-between mt-4">
-            <div class="col-6 col-md-2 col-sm-5">
+        <div class="row d-none d-md-flex justify-content-center  justify-content-md-between mt-4">
+            <div class="col-6 col-sm-5 col-md-2 ms-5">
                 <button class="btn btn-outline-light btn-lg btn-size">Clear</button>
             </div>
-            <div class="col-6 col-md-2 col-sm-5 ">
+            <div class="col-6 col-sm-5 col-md-2 me-5">
                 <button class="btn btn-outline-light btn-lg btn-size">Save</button>
+            </div>
+        </div>
+        <div class="row btn-group d-flex d-md-none justify-content-center px-5 p-md-0">
+            <div class="col-12 col-sm-6 col-md-2">
+                <button class="btn btn-outline-light btn-size">Likert</button>
+            </div>
+            <div class="col-12 col-sm-6 col-md-2">
+                <button class="btn btn-outline-light btn-size">T/F</button>
+            </div>
+            <div class="col-12 col-sm-6 col-md-2">
+                <button class="btn btn-outline-light btn-size">MCQ</button>
+            </div>
+            <div class="col-12 col-sm-6 col-md-2">
+                <button class="btn btn-outline-light text-nowrap btn-size">Short</button>
+            </div>
+        </div>
+        <div class="row d-flex d-md-none justify-content-center  justify-content-md-between mt-4">
+            <div class="col-6 col-sm-5 col-md-2">
+                <button class="btn btn-outline-light btn-size">Clear</button>
+            </div>
+            <div class="col-6 col-sm-5 col-md-2">
+                <button class="btn btn-outline-light btn-size">Save</button>
             </div>
         </div>
 </footer>
