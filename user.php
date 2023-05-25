a1e64aadd9c60447329a0748917bd4f3a03104fb
Ahmedalmannaei <ahmed.almannaie18@gmail.com>
2023-05-26 00:33:16 +0300

    hi

--- user.php
+++ user.php
@@ -4,23 +4,23 @@
 <div class="container text-center d-flex align-items-center justify-content-center">
 <div class='primary p-5'>
 <h1 class='text-white'>User profile </h1><br>
-<form>
+<form method='post' action='userVerify.php'>
 <div class="form-group">
 <label for='newUsername'class='text-white'> Change username
-            <input type="text" class="form-control" id='newUsername' value = 'Ahmed'>
+            <input type="text" class="form-control" id='newUsername' value = 'Yori' name='newUN'>
             <input type="submit"class='mt-3 btn btn-outline-light'>
             <hr>
                <label for="password" class='text-white'> New Password</label>
-               <input type="password" class="form-control" id="password">
+               <input type="password" class="form-control" id="password" name='newP'>
             </div>
             <div class="form-group">
                <label for="cPassword" class='text-white'>Confirm New Password</label>
-               <input type="password" class="form-control" id="cPassword">
+               <input type="password" class="form-control" id="cPassword" name='cnewP'>
             
                <input type="submit" class='mt-3 btn btn-outline-light'>
   <hr>            </div>
           <label for="newEmail"class='text-white'> Change Email
-          <input type="text" class="form-control" id='newEmail' value='1234@gmail.com'>
+          <input type="text" class="form-control" id='newEmail' value='1234@gmail.com' name="newEmail'>
           <input type="submit" class='mt-3 btn btn-outline-light'>
           <hr>
           
--- userVerify.php
+++ userVerify.php
@@ -7,7 +7,7 @@
 $UID=$_SESSION['activeUser'][1];
 
 if(isset($_POST['newUN'])){
-    
+   
     $username = cleanInput($_POST['newUN']);
     $unPattern = '/^[A-Za-z0-9]{3,15}$/';
     
@@ -54,7 +54,7 @@ if(isset($_POST['newEmail'])){
     $changeEM->bindParam(":email",$newemail);
     echo $emailSql;
     $changeEM->execute();
-    if($uniqueUn->rowCount() > 0) {
+    if($changeEM->rowCount() > 0) {
         die("Email already in use try a different email<br>");
 }
 $updateEmail = 'UPDATE `users` SET `email`=:email2 WHERE UID='.$UID;
@@ -62,10 +62,40 @@ $updateEmail2 = $db->prepare($updateEmail);
 $updateEmail2->bindParam(":email2",$newemail);
 $updateEmail2->execute();
 echo "Successfully updated Email <br>";
-    $db=null;
+$db=null;
 
 }
-if(isset($_POST['newP'])){
+if(isset($_POST['newP']) && isset($_POST['cnewP'])){
+    $newP = cleanInput($_POST['newP']);
+    $cnewP = cleanInput($_POST['cnewP']);
+    $pPattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%\*\-]{6,30}$/';
+    if(!preg_match($pPattern, $newP)) {
+        die("Password does not match the pattern");
+    }
+    if(!preg_match($pPattern, $cnewP)) {
+        die("Password does not match the pattern");
+    }
+    if($newP != $cnewP){
+        die("passwords not matching");
+    }
+    
+    $newP= password_hash($newP, PASSWORD_DEFAULT);
+   
+    $Psql = 'SELECT  `password`  FROM `users` WHERE password=:password;';
+    $changeP = $db->prepare($Psql);
+    $changeP->bindParam(":password",$newP);
+    $changeP->execute();
+    echo $changeP->rowCount();
+    if($changeP->rowCount() > 0) {
+        die("new password cant be old password<br>");
+}
+
+    $updateP = 'UPDATE `users` SET `password`=:password2 WHERE UID='.$UID;
+    
+$updateP2 = $db->prepare($updateP);
+$updateP2->bindParam(":password2",$newP);
+$updateP2->execute();
+echo "Successfully updated password <br>";
     
 }
 
